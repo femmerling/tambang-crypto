f43b138dbbd8601788d0d6711f9a08c092fa3ca2
Bramantyo Erlangga <bram@prismapp.io>
2017-12-11 17:04:45 +0700

    fix add retry vipbtc on error

--- babix-ngepetz/vipbtc/common.py
+++ babix-ngepetz/vipbtc/common.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 class APIResponseError(Exception):
     """ Exception raise if the API replies with an HTTP code
@@ -11,13 +12,24 @@ class Session:
         # self.__requests_session.headers.update({'User-Agent': 'medex'})
 
     def api_request(self, url, params={}, auth='', http_call='get'):
-        if http_call == 'get':
-            response = self.__requests_session.get(url)
-        elif http_call == 'post':
-            response = self.__requests_session.post(url, data=params, auth=auth)
+
+        def api_exec(url, params={}, auth='', http_call='get'):
+            if http_call == 'get':
+                response = self.__requests_session.get(url)
+            elif http_call == 'post':
+                response = self.__requests_session.post(url, data=params, auth=auth)
+            return response
+
+
+        resp = api_exec(url=url, params=params, auth=auth, http_call=http_call)
+        while not resp.ok:
+            resp = api_exec(url=url, params=params, auth=auth, http_call=http_call)
+            time.sleep(0.5)
         
-        if not response.ok:
-            msg = "API response error: %s".format(response.status_code)
-            raise APIResponseError(msg)
+        return resp
         
-        return response
+        # if not response.ok:
+        #     msg = "API response error: %s".format(response.status_code)
+        #     raise APIResponseError(msg)
+        
+        # return response
