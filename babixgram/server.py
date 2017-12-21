851ddb6217c7328913c9b137bf5da88a90d66269
sendz <sendzation@gmail.com>
2017-12-22 00:50:06 +0700

    Add comment

--- babixgram/server.py
+++ babixgram/server.py
@@ -2,7 +2,7 @@ import time
 import telepot
 from telepot.loop import MessageLoop
 
-bot = telepot.Bot('472836801:AAGQgDhB0dg471Nvqc9RjqiXZJ4K2qnieHQ')
+bot = telepot.Bot('472836801:AAGQgDhB0dg471Nvqc9RjqiXZJ4K2qnieHQ') # @BabixBot hosted by Sendy
 
 def handle(msg):
   if msg['text'] == '/start':
