feec2dd5c03e3f417360c3f74fa261f7563bd313
sendz <sendzation@gmail.com>
2017-12-22 00:46:25 +0700

    Add server listener for telegram bot

---
+++ babixgram/server.py
@@ -0,0 +1,17 @@
+import time
+import telepot
+from telepot.loop import MessageLoop
+
+bot = telepot.Bot('472836801:AAGQgDhB0dg471Nvqc9RjqiXZJ4K2qnieHQ')
+
+def handle(msg):
+  if msg['text'] == '/start':
+    id = msg['from']['id']
+    message = 'Telegram User ID: %s' % id
+    bot.sendMessage(id, message)
+
+MessageLoop(bot, handle).run_as_thread()
+print('I\'m listening...')
+
+while 1:
+  time.sleep(1)
